db9d304865ff00af81e2927dac123d057f867cfd
m00nbek <melikulov990@gmail.com>
2021-11-25 15:01:03 +0500

    ui update

--- script.js
+++ script.js
@@ -1,82 +1,62 @@
-// const socket = io("http://127.0.0.1:3000");
-const messageForm = document.getElementById("send-container");
+const sendButton = document.getElementById("send-container");
 const messageInput = document.getElementById("message-input");
-const messageContainer = document.getElementById("message-container");
-
-// const name = prompt("What is your name?");
-// appendMessage("You joined");
-// socket.emit("new-user", name);
-
-// socket.on("chat-message", (data) => {
-//   appendMessage(`${data.name}: ${data.message}`);
-// });
-// socket.on("user-connected", (name) => {
-//   appendMessage(`${name} connected`);
-// });
-// socket.on("user-disconnected", (name) => {
-//   appendMessage(`${name} disconnected`);
-// });
-
 // Create WebSocket connection.
 const socket = new WebSocket("ws://localhost:3300");
-
-// connect
-// const name = prompt("What is your name?");
-// appendMessage("You joined");
-// socket.send("new-user", name);
-
-// // message
-// socket.addEventListener("chat-message", function (event) {
-//   appendMessage(`${event.name}: ${event.message}`);
-// });
-
-
-
-
 // Listen for messages
 socket.addEventListener("message", function (event) {
-    if (typeof event.data == Blob | typeof event.data == 'object') {
-        console.log("Blob type")
-        var reader = new FileReader();
-        reader.addEventListener("loadend", function () {
-          var response = JSON.parse(reader.result);
-          appendMessage(`${response.sender}: ${response.msg}`);
-        });
-        reader.readAsText(event.data);
-    } else {
-        console.log("not Blob type")
-        var data = JSON.parse(event.data);
-        appendMessage(`${data.sender}: ${data.msg}`);
-    }
+  if ((typeof event.data == Blob) | (typeof event.data == "object")) {
+    console.log("Blob type");
+    var reader = new FileReader();
+    reader.addEventListener("loadend", function () {
+      var response = JSON.parse(reader.result);
+      appendMessage(`${response.sender}: ${response.msg}`);
+    });
+    reader.readAsText(event.data);
+  } else {
+    console.log("not Blob type");
+    var data = JSON.parse(event.data);
+    appendMessage(`${data.sender}: ${data.msg}`);
+  }
 });
-
-// listen for open
-// socket.onopen = function() {
-    
-// }
-
 // get user's name
-const name = prompt("What is your name?");
+let name = "";
+while(name == "" || name == " ") {
+  name = prompt("What is your name?");
+};
 // Send message
-messageForm.addEventListener("submit", (e) => {
-    e.preventDefault();
-
-    // get message from document
-    const message = messageInput.value;
-    // create json 
+sendButton.addEventListener("click", (e) => {
+  e.preventDefault();
+  sendMessage();
+});
+// send message
+function sendMessage() {
+  // get message from document
+  const message = messageInput.value;
+  if (message != "" && message != " ") {
+    // create json
     let data = {
-        sender: name,
-        msg: message
-    }
+      sender: name,
+      msg: message,
+    };
     appendMessage(`${data.sender}: ${data.msg}`);
     socket.send(JSON.stringify(data));
 
     // ui
     messageInput.value = "";
-  });
+  }
+}
+// send message when enter pressed
+messageInput.addEventListener("keydown", (e) => {
+  if (e.keyCode == 13) {
+    // enter pressed
+    sendMessage();
+  }
+});
+
 // append message to document
 function appendMessage(message) {
-  const messageElement = document.createElement("div");
-  messageElement.innerText = message;
-  messageContainer.append(messageElement);
+    const chatWindow = document.getElementById('chat-window')
+    const displayMessage = document.getElementById("display-message");
+    displayMessage.innerHTML += `<p><strong>${message}</strong></p>`;
+    chatWindow.scrollTop = chatWindow.scrollHeight;
 }
